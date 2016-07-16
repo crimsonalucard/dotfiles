ad4852a5ed864670ea006285737c74bb2d758210
byeh <byeh@boingo.com>
2016-07-15 17:17:22 -0700

    added auto install plugins

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ cp -r vimrc/ ~/
 cp -r bashrc/ ~/
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git pull https://github.com/VundleVim/Vundle.vim.git
+vim -c "PluginInstall" -c "q" -c "q" -c "q"
 
+echo "finished installing"
 
 
 
