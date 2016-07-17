bbeddd851a7a67162c2a66c635ffa9d9e36c3634
Brian <heynairb@gmail.com>
2016-07-17 00:56:48 -0700

    fonts

--- install.sh
+++ install.sh
@@ -7,6 +7,14 @@ cp -r vimrc/ ~/
 cp -r bashrc/ ~/
 cp -r tmux/ ~/
 
+#install powerline fonts
+mkdir fonts
+cd fonts
+git clone git@github.com:powerline/fonts.git
+git pull origin master
+cd -
+
+
 #install bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cd ~/.vim/bundle/Vundle.vim && git pull https://github.com/VundleVim/Vundle.vim.git && cd -
@@ -19,6 +27,10 @@ vim -c "PromptlineSnapshot ~/.shell_prompt.sh" -c "q" -c "q"
 
 #reset bash
 source ~/.bashrc
+
+tmux
+
+vim -c "q" -c "q"
 
 echo "finished installing"
 
